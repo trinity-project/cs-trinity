9e31a64e8a3aa13401cddacef0fa9991eaf564c5
xiaohuta <xiaohuta@github.com>
2019-04-24 11:49:36 +0800

    DEV: Change the channel class: 1. add new method; 2. change the members' names; 3. remove useless method;

--- cs-trinity/Channel/Channel.cs
+++ cs-trinity/Channel/Channel.cs
@@ -38,86 +38,46 @@ namespace Trinity.Channel
 {
     class Channel
     {
-        private string founder = null;
-        private string partner = null;
-        private string founder_pubkey = null;
-        private string founder_address = null;
-        private string partner_pubkey = null;
-        private string partner_address = null;
-        private string asset_type = null;
-        private string channel_name = null;
-        private Dictionary<string, JObject> deposit;
+        private string Founder;
+        private string Partner = null;
+        private string FounderPubKey = null;
+        private string FounderAddress = null;
+        private string PartnerPubKey = null;
+        private string PartnerAddress = null;
+        private string AssetType = null;
+        private string ChannelName = null;
+        private Dictionary<string, Dictionary<string, double>> Deposit;
+        private Dictionary<string, Dictionary<string, double>> Balance;
 
-        public Channel(string _founder, string _partner)
+        public Channel(string founder, string partner)
         {
-            founder = _founder;
-            partner = _partner;
-            //founder_pubkey = founder.split("@")[0];
-            //founder_address = pubkey_to_address(founder_pubkey);
-            //partner_pubkey = partner.split("@")[0];
-            //partner_address = pubkey_to_address(partner_pubkey);
+            this.Founder = founder;
+            this.Partner = partner;
         }
 
-        public static string[] getChannel(string address1, string address2)
+        public Channel(string ChannelName)
+        {
+            this.ChannelName = ChannelName;
+        }
+
+        public static string[] GetChannel(string founder, string partner)
         {
             string[] channel = { "test" };
             return channel;
         }
 
-        private string init_channle_name()
+        public string NewChannel()
         {
-            string encodeStr = founder + partner + DateTime.Now.ToString();
+            string encodeStr = this.Founder + this.Partner + DateTime.Now.ToString();
             byte[] sourcebytes = Encoding.Default.GetBytes(encodeStr);
             MD5 md5 = new MD5CryptoServiceProvider();
             byte[] hashedbytes = md5.ComputeHash(sourcebytes);
             return BitConverter.ToString(hashedbytes).Replace("-", "");
         }
 
-        public string create(string _assetType, UInt16 _deposit, string comment = "", string _channel_name = "")
+        public void Commit()
         {
-            JObject message = new JObject(); 
-            JObject messageBody = new JObject();
-            JObject subitem = new JObject();
-            JObject deposit = new JObject();
-
-            /*
-            string[] ch = Channel.getChannel(founder_pubkey, partner_pubkey);
-            if (ch.Length != 0)
-            {
-                Console.WriteLine("the channel have exist");
-                return null;
-            }
-
-            if (0 <= _deposit)
-            {
-                Console.WriteLine("Could not trigger register channel because of illegal deposit:{0}", _deposit.ToString());
-            }
-            */
-
-            asset_type = _assetType;            
-            subitem[asset_type] = _deposit;
-            deposit[founder] = subitem;
-            deposit[partner] = subitem;
-
-            channel_name = _channel_name == "" ? init_channle_name() : _channel_name;
-
-            //result = APIChannel.add_channel(channel_name, founder, partner, EnumChannelState.INIT.name, 0, deposit, Channel.magic_number);
-
-            message["MessageType"] = "RegisterChannel";
-            message["Sender"] = founder;
-            message["Receiver"] = partner;
-            message["ChannelName"] = channel_name;
-            message["NetMagic"] = "12345";//TODO : get Net Magic;
-
-            messageBody["AssetType"] = _assetType;
-            messageBody["Deposit"] = deposit;
-            messageBody["Comments"] = comment;
-
-            message["MessageBody"] = messageBody;
-
-            return message.ToString();
-
-            //TODO  send the message to gateway
+            
         }
     }
 }
