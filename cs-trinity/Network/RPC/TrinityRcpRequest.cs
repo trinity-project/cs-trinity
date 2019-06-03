ff361bbed553d4add789124b80dc566208b0c67e
yilwhust <yilwhust@163.com>
2019-06-03 17:57:20 +0800

    add rpc interface and getRouterInfo data class

---
+++ cs-trinity/Network/RPC/TrinityRcpRequest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using Neo.IO.Json;
+using Trinity.Wallets.Templates.Messages;
+using Trinity.Wallets;
+
+namespace Trinity.Network.RPC
+{
+    public class TrinityRpcRequest
+    {
+        public static string Post<T>(string gateWayUri, string messagType, T TRequest)
+        {
+            string result = null;
+
+            List<T> parameters = new List<T>() { TRequest };
+            JObject request = new JObject();
+            request["jsonrpc"] = "2.0";
+            request["id"] = 1;
+            request["method"] = messagType;
+            request["params"] = JObject.Parse(parameters.Serialize());
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(gateWayUri);
+            req.Method = "POST";
+            req.ContentType = "application/json";
+            req.Timeout = 10000; //timeout is 10 seconds
+
+            #region add parameters           
+            byte[] data = Encoding.UTF8.GetBytes(request.ToString());
+            req.ContentLength = data.Length;
+            using (Stream reqStream = req.GetRequestStream())
+            {
+                reqStream.Write(data, 0, data.Length);
+                reqStream.Close();
+            }
+            #endregion
+
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            Stream stream = resp.GetResponseStream();
+            //获取响应内容
+            using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                result = reader.ReadToEnd();
+            }
+            return result;
+        }
+    }
+}
